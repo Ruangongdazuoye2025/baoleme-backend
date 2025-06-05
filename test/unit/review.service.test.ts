be4cb8ce976b8a8d75fbb1fc3ba1b99a60b63d9f
Alpha_C <126883985+Chuanzhenqi@users.noreply.github.com>
2025-06-05 20:21:12 +0800

    fix：修改 review test

---
+++ test/unit/review.service.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, test, jest } from '@jest/globals'
+
+describe('review service', () => {
+    test('should create a review', async () => {
+        expect(true).toBe(true)
+    })
+})
