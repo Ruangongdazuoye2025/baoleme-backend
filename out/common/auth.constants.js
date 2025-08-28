5f929c185e92a7eae27e7c2d8ff3411943a7aaae
CrSjimo <crs_16423@outlook.com>
2025-08-28 09:08:28 +0800

    remove unignored files

--- out/common/auth.constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PASSWORD_MIN_LENGTH = exports.SALT_ROUNDS = exports.USER_NAME_ID_LENGTH = exports.USER_NAME_PREFIX = void 0;
-exports.USER_NAME_PREFIX = 'User ';
-exports.USER_NAME_ID_LENGTH = 8;
-exports.SALT_ROUNDS = 10;
-exports.PASSWORD_MIN_LENGTH = 6;
-//# sourceMappingURL=auth.constants.js.map
