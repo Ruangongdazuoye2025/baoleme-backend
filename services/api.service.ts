33d44b6df99dc37ee9ef1c06881eca4e3b984f03
rachioff <15523001+rachioff@user.noreply.gitee.com>
2025-08-29 10:14:45 +0800

    feature: establish order service

--- services/api.service.ts
+++ services/api.service.ts
@@ -18,6 +18,7 @@ const ApiService: ServiceSchema<ApiSettingsSchema> = {
                 mappingPolicy: "restrict",
                 authorization: true,
                 aliases: {
+                    // Auth
                     "POST /auth/register": "auth.register",
                     "POST /auth/login": "auth.login",
                     "POST /auth/forgot-password": "auth.forgotPassword",
@@ -27,17 +28,31 @@ const ApiService: ServiceSchema<ApiSettingsSchema> = {
                     "POST /auth/update-email": "auth.updateEmail",
                     "POST /auth/update-password": "auth.updatePassword",
 
+                    // User
                     "GET /user/:id": "user.get",
                     "PATCH /user/:id/profile": "user.updateProfile",
                     "PATCH /user/:id/avatar": "multipart:user.uploadAvatar",
                     "DELETE /user/:id/avatar": "user.deleteAvatar",
 
+                    // Address
                     "POST /addresses": "address.addAddress",
                     "GET  /addresses": "address.getAddresses",
                     "GET  /addresses/:id": "address.getAddressById",
                     "PATCH  /addresses/:id": "address.updateAddress",
                     "PATCH /addresses/:id/pos": "address.updateAddressOrder",
                     "DELETE /addresses/:id": "address.deleteAddress",
+
+                    // Order
+                    "GET /orders/as-customer": "order.getOrdersAsCustomer",
+                    "GET /orders/as-shop/:shopId": "order.getOrdersAsShop",
+                    "GET /orders/as-rider": "order.getOrdersAsRider",
+                    "GET /orders": "order.getOrders",
+                    "GET /orders/:id": "order.getOrderById",
+                    "POST /orders": "order.createOrder",
+                    "PATCH /orders/:id/rider": "order.updateOrderRider",
+                    "PATCH /orders/:id/status": "order.updateOrderStatus",
+                    "PATCH /orders/:id/delivery": "order.updateOrderDelivery",
+                    "DELETE /orders/:id": "order.deleteOrder",
                 },
                 onAfterCall(ctx, route, req, res, data) {
                     if (!data) {
---
+++ services/order.service.ts
@@ -0,0 +1,15 @@
+import { Context, ServiceSchema, Errors } from "moleculer";
+
+const OrderService: ServiceSchema = {
+    name: "order",
+
+    settings: {
+
+    },
+
+    actions: {
+        
+    }
+}
+
+export default OrderService;
