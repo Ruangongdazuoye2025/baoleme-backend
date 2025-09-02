30537eb03d3d34f2e6a7e3b37d077e6b4946c4a8
CrSjimo <crs_16423@outlook.com>
2025-09-02 14:32:18 +0800

    add prisma migrate reset before all tests

--- tests/integration/setup.ts
+++ tests/integration/setup.ts
@@ -1,4 +1,5 @@
 import { SmtpTestServer } from "../utils/smtp.util";
+import { execSync } from "child_process"
 
 const SMTP_PORT = 1025;
 const SMTP_HOST = '127.0.0.1';
@@ -8,6 +9,7 @@ const smtpServer = new SmtpTestServer(SMTP_PORT, SMTP_HOST);
 
 beforeAll(async () => {
     await smtpServer.start(SMTP_PORT, SMTP_HOST);
+    execSync('npx prisma migrate reset -f --skip-generate');
 }, 30000);
 
 afterAll(async () => {
